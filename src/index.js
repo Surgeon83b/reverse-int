bd6efda34563f4ac401e3d6608bb2df2ae364d7e
Surgeon83b <89166001+Surgeon83b@users.noreply.github.com>
2022-06-22 11:02:06 +0300

    init & fix )

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let res = 0;
+  let nn = Math.abs(n);
+
+  while (nn > 0) {
+    res = res * 10 + nn % 10;
+    nn = Math.trunc(nn / 10);
+  }
+  console.log(res);
+  return res;
 }
+
